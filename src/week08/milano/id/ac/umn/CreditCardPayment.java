6d2ccb249a9f9e6c5ec2c3c62787ea0e468da753
Milano0505 <milanopandey345@gmail.com>
2025-10-29 11:20:15 +0700

    class CreditCardPayment and BankTransferPayment

---
+++ src/week08/milano/id/ac/umn/CreditCardPayment.java
@@ -0,0 +1,14 @@
+package week08.milano.id.ac.umn;
+
+public class CreditCardPayment extends Payment {
+    private String cardNumber;
+    public CreditCardPayment(double amount, String cardNumber) {
+        super(amount);
+        this.cardNumber = cardNumber;
+    }
+
+    @Override
+    void processPayment() {
+        System.out.println("Processing credit card payment of $" + amount + " for card number " + cardNumber);
+    }
+}
